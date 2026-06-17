3e7433a9af71f73cd7c336e653860c1cf0826173
Dell <Dell@DESKTOP-OM1EPSE>
2026-06-17 14:00:53 +0530

    Third commit for jenkinsss

--- JenkinsDemo/src/test/java/ScripttoRunFromJenkins.java
+++ JenkinsDemo/src/test/java/ScripttoRunFromJenkins.java
@@ -7,7 +7,7 @@ public class ScripttoRunFromJenkins {
 	public void jenkinsDemo() {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
-		driver.manage().window().maximize(); //maximze window	
+		driver.manage().window().maximize(); //maximzeee window	
 		}
 	
 }
