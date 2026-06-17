eb0e9d13a2c164fa90b6e40edae27c6db9f7ab02
Dell <Dell@DESKTOP-OM1EPSE>
2026-06-17 13:09:59 +0530

    First commit for jenkins

---
+++ JenkinsDemo/src/test/java/ScripttoRunFromJenkins.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ScripttoRunFromJenkins {
+ @Test
+	public void jenkinsDemo() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();	
+		}
+	
+}
